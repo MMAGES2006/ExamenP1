2b7a25024576ee18f018e27042d755923eedbbef
MMAGES2006 <22170@portalcedi.edu.mx>
2025-02-11 09:04:54 -0600

    Formulario y service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -8,6 +10,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  items: {movie: string; date: string; Producer: string; Pcasa: string; main: string}[] = [];
+  nextId: number = 1;  
+
+  constructor(public navCtrl: NavController) {}
+
+
+  addItem(movie: string, date: string, Producer: string, Pcasa: string, main: string): void 
+  {
+    if (movie.trim() && date.trim() && Producer.trim() && Pcasa.trim() && main.trim()) {
+      this.items.push({
+        movie: movie.trim(),
+        date: date.trim(),
+        Producer: Producer.trim(),
+        Pcasa: Pcasa.trim(),
+        main: main.trim(),
+        
+
+      });
+    } else {
+      console.error('El nombre y la fecha no pueden estar vacios');
+    }
+  }
 
 }
